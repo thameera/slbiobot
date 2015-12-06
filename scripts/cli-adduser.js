d3919b339213dd7b3d5a5a3d68ae187ac96bc688
Thameera Senanayaka <thameera123@gmail.com>
2015-12-06 20:56:27 +0530

    Implement cli-adduser for adding new users to users.txt

---
+++ scripts/cli-adduser.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+'use strict';
+
+/*
+ * cli-adduser
+ * Adds given users to users.txt if they don't already exist there
+ * USAGE: ./cli-adduser.js user1 user2 ...
+ */
+
+const program = require('commander');
+const chalk = require('chalk');
+
+const BiosReader = require('./lib/bios-reader');
+const readConfig = require('./lib/read-config');
+const Twitter = require('./lib/twitter');
+const Userlist = require('./lib/userlist');
+
+const biosReader = new BiosReader();
+const config = readConfig().Twitter;
+const twitter = new Twitter(config);
+const userlist = new Userlist();
+
+let usernames;
+
+const error = msg => {
+  console.error(chalk.red(`ERROR: ${msg}`));
+};
+
+const warn = msg => {
+  console.error(chalk.yellow(msg));
+};
+
+const info = msg => {
+  console.log(chalk.cyan(msg));
+}
+
+program
+  .arguments('[username...]')
+  .action( (names) => { usernames = names; } )
+  .parse(process.argv);
+
+if (!usernames || !usernames.length) {
+  error('No usernames given');
+  process.exit(1);
+}
+
+usernames.forEach( username => {
+  if (biosReader.usernameExists(username)) {
+    warn(`'${username}' already exists`);
+    return;
+  }
+
+  twitter.getIDByScreenName(username)
+    .then( id => {
+      userlist.appendUser(id);
+      userlist.save();
+      info(`'${username}' added`);
+    })
+    .catch( err => {
+      error(err);
+    });
+});
+
